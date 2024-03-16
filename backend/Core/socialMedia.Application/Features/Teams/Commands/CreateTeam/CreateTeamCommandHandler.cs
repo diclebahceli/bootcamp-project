f33a2a25f02c6ca00b40f3375f9ee99bc6290085
diclebahceli <diclebahceli24@gmail.com>
2024-03-16 23:31:52 +0300

    feat: add create operation

---
+++ backend/Core/socialMedia.Application/Features/Teams/Commands/CreateTeam/CreateTeamCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using socialMedia.Domain;
+
+namespace socialMedia.Application;
+
+public class CreateTeamCommandHandler : IRequestHandler<CreateTeamCommandRequest>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public CreateTeamCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+
+    }
+
+    public async Task Handle(CreateTeamCommandRequest request, CancellationToken cancellationToken)
+    {
+        Team team = new(request.Name, request.Description, request.Image);
+        await _unitOfWork.GetWriteRepository<Team>().AddAsync(team);
+        await _unitOfWork.SaveAsync();
+    }
+
+}
---
+++ backend/Core/socialMedia.Application/Features/Teams/Commands/CreateTeam/CreateTeamCommandRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace socialMedia.Application;
+
+public class CreateTeamCommandRequest : IRequest
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string? Image { get; set; }
+
+
+}
