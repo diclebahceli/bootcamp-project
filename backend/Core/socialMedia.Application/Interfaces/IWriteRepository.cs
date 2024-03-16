fc1da10a46910f1f9fba15470170f26539aaef84
diclebahceli <diclebahceli24@gmail.com>
2024-03-16 16:15:53 +0300

    feat: add generic write repository

--- backend/Core/socialMedia.Application/Interfaces/IWriteRepository.cs
+++ backend/Core/socialMedia.Application/Interfaces/IWriteRepository.cs
@@ -4,5 +4,11 @@ namespace socialMedia.Application;
 
 public interface IWriteRepository<T> where T : class, IEntityBase, new()
 {
+    Task AddAsync(T entity);
 
+    Task AddRangeAsync(IList<T> entities);
+
+    Task<T> UpdateAsync(T entity);
+
+    Task HardDeleteAsync(T entity);
 }
--- backend/Infrastructure/socialMedia.Persistence/Repositories/WriteRepository.cs
+++ backend/Infrastructure/socialMedia.Persistence/Repositories/WriteRepository.cs
@@ -1,6 +1,39 @@
-﻿namespace socialMedia.Persistence;
+﻿using Microsoft.EntityFrameworkCore;
+using socialMedia.Application;
+using socialMedia.Domain;
+
+namespace socialMedia.Persistence;
 
 public class WriteRepository<T> : IWriteRepository<T> where T : class, IEntityBase, new()
 {
+    private readonly DbContext dbContext;
+
+    public WriteRepository(DbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    private DbSet<T> Entities => dbContext.Set<T>();
+
+    public async Task AddAsync(T entity)
+    {
+        await Entities.AddAsync(entity);
+    }
+
+    public async Task AddRangeAsync(IList<T> entities)
+    {
+        await Entities.AddRangeAsync(entities);
+    }
+    public async Task<T> UpdateAsync(T entity)
+    {
+        //update processes cannot be run asynchronously so we can combine it with Run
+        await Task.Run(() => Entities.Update(entity));
+        return entity;
+    }
+
+    public async Task HardDeleteAsync(T entity)
+    {
+        await Task.Run(() => Entities.Remove(entity));
+    }
 
 }
