80acf8ef10431b90731b4e6a4c95defbd77b6501
diclebahceli <diclebahceli24@gmail.com>
2024-03-16 23:32:11 +0300

    feat: add update operation for team entity

---
+++ backend/Core/socialMedia.Application/Features/Teams/Commands/UpdateTeam/UpdateTeamCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using socialMedia.Domain;
+
+namespace socialMedia.Application;
+
+public class UpdateTeamCommandHandler : IRequestHandler<UpdateTeamCommandRequest>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    public UpdateTeamCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        this._unitOfWork = unitOfWork;
+        this._mapper = mapper;
+    }
+    public async Task Handle(UpdateTeamCommandRequest request, CancellationToken cancellationToken)
+    {
+        var team = await _unitOfWork.GetReadRepository<Team>().GetAsync(x => x.Id == request.Id && !x.IsDeleted, enableTracking: true);
+        var newObject = _mapper.Map<Team, UpdateTeamCommandRequest>(request);
+        team.Name = newObject.Name;
+        team.Description = newObject.Description;
+        team.Image = newObject.Image;
+        await _unitOfWork.GetWriteRepository<Team>().UpdateAsync(team);
+        await _unitOfWork.SaveAsync();
+    }
+
+}
---
+++ backend/Core/socialMedia.Application/Features/Teams/Commands/UpdateTeam/UpdateTeamCommandRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace socialMedia.Application;
+
+public class UpdateTeamCommandRequest : IRequest
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string? Image { get; set; }
+}
