1fb9bad9d1e8e85aa5d98739b7dc6b2702dcb3fd
diclebahceli <diclebahceli24@gmail.com>
2024-03-24 15:00:38 +0300

    feat: add like count

--- frontend/hobby-hub/src/app/Components/post.tsx
+++ frontend/hobby-hub/src/app/Components/post.tsx
@@ -1,20 +1,32 @@
+"use client";
+import React from "react";
+import { useState } from "react";
 import { Post } from "../Models/post";
 import { GetUserById } from "../services/UserService";
 import { FaThumbsUp, FaComments } from "react-icons/fa";
+import { User } from "../Models/user";
 
-export default async function Post({ post }: { post: Post }) {
-  const userName = (await GetUserById(post.userId)).name;
+export default function Post({ post, user }: { post: Post; user: User }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  const handleLikeClick = () => {
+    setLikeCount(likeCount + 1);
+  };
   return (
     <div className="col-md-12 mb-4">
       <div className="card">
-        <img src={post.Image} className="card-img-top" alt={userName} />
+        <img src={post.Image} className="card-img-top" alt={user.name} />
         <div className="card-body">
-          <h5 className="card-title">{userName}</h5>
+          <h5 className="card-title">{user.name}</h5>
           <p className="card-text">{post.Description}</p>
           <div className="d-flex justify-content-between">
             <div>
-              <button className="btn btn-outline-primary mr-2">
-                <FaThumbsUp /> Like
+              <button
+                className="btn btn-outline-primary mr-2"
+                onClick={handleLikeClick}
+              >
+                <FaThumbsUp />
+                {likeCount} Like
               </button>
               <button className="btn btn-outline-primary">
                 <FaComments /> Comment
--- frontend/hobby-hub/src/app/pages/community/[id]/page.tsx
+++ frontend/hobby-hub/src/app/pages/community/[id]/page.tsx
@@ -1,18 +1,24 @@
 import Post from "@/app/Components/post";
 import { GetAllPosts, GetPostByUserId } from "@/app/services/PostService";
+import { GetUserById } from "@/app/services/UserService";
 
 export default async function Community({
   params,
 }: {
-  params: { id: string };
+  params: { id: number };
 }) {
   const Posts = await GetPostByUserId(params.id);
   return (
     <div className="container">
       <h1 className="my-4">Communication Detail Page</h1>
-      {Posts.map((post) => (
+      {Posts.map(async (post) => (
         <div className="row" key={post.id}>
-          <Post post={post} key={post.id} {...post} />
+          <Post
+            post={post}
+            user={await GetUserById(post.userId)}
+            key={post.id}
+            {...post}
+          />
         </div>
       ))}
     </div>
